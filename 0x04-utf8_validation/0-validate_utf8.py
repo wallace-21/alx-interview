41c665061757a2a10943a56d5bb677c0e362b960
wallace <exnesswallace@gmail.com>
2024-05-24 04:40:05 +0200

    added to conditions to check the bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,8 +5,25 @@
 
 def validUTF8(data):
     """acceepts a list of integers"""
+    bytes_of_num = 0
     for nums in data:
         if nums > 255:
             return False
+
+        if bytes_of_num == 0:
+            """check how many bytes the current number indicates"""
+            if (nums >> 5) == 0b110:
+                bytes_of_num = 1
+            elif (nums >> 4) == 0b1110:
+                bytes_of_num = 2
+            elif (nums >> 3) == 0b11110:
+                bytes_of_num = 3
+            elif (nums >> 7):
+                return False
+        else:
+            """Check that the next bytes are continuation bytes"""
+            if (nums >> 6) != 0b10:
+                return False
+            bytes_of_num -= 1
 
     return True
