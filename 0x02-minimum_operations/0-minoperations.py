6ee362e4782b9c22ba4d52ca509b8cd215a8213e
wallace <exnesswallace@gmail.com>
2024-06-15 14:28:19 +0200

    added a codition that returns 0 when and negative numbers are passed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,8 +15,10 @@ def minOperations(n):
     """
     prime_used = []
     primes = [2, 3, 5, 7, 11, 13, 17, 19, 21, 23, 29, 31, 37, 41, 43, 47]
-    if n is 0:
+
+    if n == 0 or n < 0:
         return (0)
+
     for prime in primes:
         while n % prime == 0:
 
