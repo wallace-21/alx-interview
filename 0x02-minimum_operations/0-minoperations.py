7ecbbd0294441dc0dbe431026c76b164fb759f54
wallace <exnesswallace@gmail.com>
2024-06-15 13:52:10 +0200

    function that returns a min operations needed to result in exactly n H characters

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def minOperations(n):
+    """a function given calculates the min
+       number of operations needed
+       Returns:
+         - returns the sum of prime factors
+           of (n)
+    """
+
+    """ stores the prime numbers used to divide (n) with
+    """
+    prime_used = []
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 21, 23, 29, 31, 37, 41, 43, 47]
+
+    for prime in primes:
+        while n % prime == 0:
+
+            prime_used.append(prime)
+            n //= prime
+
+        if n == 1:
+            break
+    return(sum(prime_used))
