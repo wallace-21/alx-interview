a513eced377650e826589ca443915ae2e420dbae
wallace <exnesswallace@gmail.com>
2024-06-19 20:04:09 +0200

    revert back

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 
+"""import typing, the type anotation module"""
+from typing import List
+
 
 def makeChange(coins, total):
     """functiom that determines the fewest number of
        coins needed to meet a given amount totat
-
         Args:
           coins: List[int] = is a list with coins used to get total.
           total: int = the number that coins should add up too.
-
         Return:
           - returns the length of the number of coins used
     """
     coins.sort(reverse=True)
-    result = []
+    result: List[int] = []
 
     if total <= 0:
         return (0)
-
     for coin in coins:
         while total >= coin:
             result.append(coin)
             total -= coin
-
             if total == 0:
                 break
     """ return -1 If total cannot be met by
@@ -30,5 +29,4 @@ def makeChange(coins, total):
     """
     if total > 0:
         return (-1)
-
     return (len(result))
