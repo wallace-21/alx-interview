698b22b59ecba8acab3a6a7785c6aae25d1645fa
wallace <exnesswallace@gmail.com>
2024-07-31 15:09:52 +0200

    used semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,42 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request')
 
 // Star Wars API base URL (template)
-const baseUrl = 'https://swapi.dev/api/films/';
+const baseUrl = 'https://swapi.dev/api/films/'
 
 // Movie ID from command-line argument
-const movieId = parseInt(process.argv[2]);
+const movieId = parseInt(process.argv[2])
 
 // Construct the URL for fetching movie data
-const movieUrl = `${baseUrl}${movieId}/`;
+const movieUrl = `${baseUrl}${movieId}/`
 
 // Fetch movie data
 request(movieUrl, (error, response, body) => {
   if (error) {
-    console.error('Error: Unable to fetch movie data');
-    process.exit(1);
+    console.error('Error: Unable to fetch movie data')
+    process.exit(1)
   }
 
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters || [];
+  const movieData = JSON.parse(body)
+  const characters = movieData.characters || []
   const characterRequests = characters.map(characterUrl => new Promise((resolve, reject) => {
     request(characterUrl, (error, response, body) => {
       if (error) {
-        reject(`Error: Unable to fetch character data for URL ${characterUrl}`);
+        reject(new Error(`Error: Unable to fetch character data for URL ${characterUrl}`))
       } else {
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
+        const characterData = JSON.parse(body)
+        resolve(characterData.name)
       }
-    });
-  }));
+    })
+  }))
 
   Promise.all(characterRequests)
     .then(names => {
-      names.forEach(name => console.log(name));
+      names.forEach(name => console.log(name))
     })
     .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
-});
+      console.error(error.message)
+      process.exit(1)
+    })
+})
