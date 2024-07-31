2e8fb1c29549fe43060cbf4213e145ead305d2a8
wallace <exnesswallace@gmail.com>
2024-07-31 15:12:41 +0200

    used semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,42 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request'); // Added semicolon
 
 // Star Wars API base URL (template)
-const baseUrl = 'https://swapi.dev/api/films/'
+const baseUrl = 'https://swapi.dev/api/films/'; // Added semicolon
 
 // Movie ID from command-line argument
-const movieId = parseInt(process.argv[2])
+const movieId = parseInt(process.argv[2]); // Added semicolon
 
 // Construct the URL for fetching movie data
-const movieUrl = `${baseUrl}${movieId}/`
+const movieUrl = `${baseUrl}${movieId}/`; // Added semicolon
 
 // Fetch movie data
 request(movieUrl, (error, response, body) => {
   if (error) {
-    console.error('Error: Unable to fetch movie data')
-    process.exit(1)
+    console.error('Error: Unable to fetch movie data'); // Added semicolon
+    process.exit(1); // Added semicolon
   }
 
-  const movieData = JSON.parse(body)
-  const characters = movieData.characters || []
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters || [];
   const characterRequests = characters.map(characterUrl => new Promise((resolve, reject) => {
     request(characterUrl, (error, response, body) => {
       if (error) {
-        reject(new Error(`Error: Unable to fetch character data for URL ${characterUrl}`))
+        reject(new Error(`Error: Unable to fetch character data for URL ${characterUrl}`)); // Added semicolon
       } else {
-        const characterData = JSON.parse(body)
-        resolve(characterData.name)
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
       }
-    })
-  }))
+    });
+  }));
 
   Promise.all(characterRequests)
     .then(names => {
-      names.forEach(name => console.log(name))
+      names.forEach(name => console.log(name)); // Added semicolon
     })
     .catch(error => {
-      console.error(error.message)
-      process.exit(1)
-    })
-})
+      console.error(error.message); // Added semicolon
+      process.exit(1); // Added semicolon
+    });
+});
